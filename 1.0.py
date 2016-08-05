e96941d89636dd382090e502a1b00727f9572472
Macsgl <317857539@qq.com>
2016-08-05 15:41:14 +0800

    增加运行日志

--- 1.0.py
+++ 1.0.py
@@ -4,10 +4,15 @@ import re
 import MySQLdb
 import urllib2
 import time
+import logging
 from bs4 import BeautifulSoup
+#log_filename = 'error.log'
+#log_format = '%(asctime)s - %(filename) - %(levelname)'
+#logging.basicConfig(filename=log_filename,format=log_format,filemode='a',level=logging.DEBUG)
 while True:
+	#try:
 	i = 0
-	l = 0
+	addUrlNum = 0
 	req = urllib2.urlopen('http://sou.zhaopin.com/jobs/searchresult.ashx?jl=%E6%9D%AD%E5%B7%9E&kw=php%E5%AE%9E%E4%B9%A0%E7%94%9F&sm=0&p=1')
 	buf = req.read()
 	soup = BeautifulSoup(buf,"html5lib")
@@ -20,12 +25,24 @@ while True:
 			sql = "INSERT INTO urls (URL) VALUES ('%s')" %(link['href'])
 			cur.execute(sql)
 			conn.commit()
-			l += 1
-	if l == 0:
-		print "It's all in db."
+			addUrlNum += 1
+	if addUrlNum == 0:
+		f = open('record.log','a')
+		note = "It's all in db." + time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
+		f.write(note+'\r\n')
+		#f.write("\n")
+		f.close()
+		#print "It's all in db."
 	else:
-		print "Add %d urls" %(l)
+		f = open('record.log','a')
+		note = "Add %d urls" %(addUrlNum) + time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
+		f.write(note)
+		#f.write("\n")
+		f.close()
+		#print "Add %d urls" %(addUrlNum)
 	cur.close()
 	conn.commit()
 	conn.close()
 	time.sleep(60)
+	#except:
+		#logging.exception("Exception Logged")
